b05f11bc7ec07d20e08aac591349cf2fb4fb6f55
Yehor Vahin <davyjohns322@gmail.com>
2025-05-04 21:15:13 +0300

    added new components forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,86 @@
-import { React } from 'react'
+import { React, useState } from 'react'
 import './App.css'
+import LoginForm from './components/LoginForm/LoginForm'
+import SearchBar from './components/SearchBar/SearchBar';
+import LangSwitcher from './components/LangSwitcher/LangSwitcher';
+import FormLogin from './components/FormLogin/FormLogin';
 
 
 
 function App() {
   
-  const handleSubmit = (evt) => {
-    evt.preventDefault();
-    console.log(evt);
+  // const handleSubmit = (evt) => {
+  //   evt.preventDefault();
+  //   console.log(evt);
+  // };
+  const [lang, setLang] = useState("uk");
+  const handleLogin = (userData) => {
+    // Виконуємо необхідні операції з даними
+    console.log(userData);
+  };
+  const [coffeeSize, setCoffeeSize] = useState("sm");
+  const handleSizeChange = (evt) => {
+    setCoffeeSize(evt.target.value);
+  };
+  const [hasAccepted, setHasAccepted] = useState(false);
+
+  const handleChange = (evt) => {
+    setHasAccepted(evt.target.checked);
   };
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-      <input type="text" name="login" />
-      <input type="password" name="password" />
-      <button type="submit">Log in</button>
-    </form>
-    </>
+    <div>
+    <h1>Please login to your account!</h1>
+      <LoginForm onLogin={handleLogin}/>
+      <SearchBar/>
+      <p>Selected language: {lang}</p>
+      <LangSwitcher value={lang} onSelect={setLang}/>
+      <div>
+      <h1>Select coffee size</h1>
+      <label>
+        <input
+          type="radio"
+          name="coffeeSize"
+          value="sm"
+          checked={coffeeSize === "sm"}
+          onChange={handleSizeChange}
+        />
+        Small
+      </label>
+      <label>
+        <input
+          type="radio"
+          name="coffeeSize"
+          value="md"
+          checked={coffeeSize === "md"}
+          onChange={handleSizeChange}
+        />
+        Meduim
+      </label>
+      <label>
+        <input
+          type="radio"
+          name="coffeeSize"
+          value="lg"
+          checked={coffeeSize === "lg"}
+          onChange={handleSizeChange}
+        />
+        Large
+      </label>
+      </div>
+      <p>
+        <b>Selected size: </b>{coffeeSize}
+      </p>
+      <div>
+      <label>
+        <input type="checkbox" name="terms"  checked={hasAccepted} onChange={handleChange}/> 
+				I accept terms and conditions
+      </label>
+      <button type="button" disabled={!hasAccepted}>
+        Proceed
+      </button>
+    </div>
+    <FormLogin/>
+    </div>
   )
 }
 
---
+++ src/components/FormLogin/FormLogin.jsx
@@ -0,0 +1,22 @@
+import {useState} from 'react'
+
+const FormLogin = () => {
+    const [values, setValues] = useState({
+        login: "",
+        password: "",
+    });
+    const handleChange = (evt) => {
+        setValues({
+            ...values,
+            [evt.target.name]: evt.target.value,
+        })
+    }
+    return(
+        <form>
+            <input type="text" name="login" value={values.login} onChange={handleChange} />
+            <input type="password" name="password" value={values.password} onChange={handleChange} />
+            <button type='submit'>Login</button>
+        </form>
+    );
+};
+export default FormLogin
