2456e73716dc2db11fb155fff320f55fe2988858
Yehor Vahin <davyjohns322@gmail.com>
2025-05-04 21:27:16 +0300

    1

--- src/components/FormLogin/FormLogin.jsx
+++ src/components/FormLogin/FormLogin.jsx
@@ -10,9 +10,18 @@ const FormLogin = () => {
             ...values,
             [evt.target.name]: evt.target.value,
         })
+    };
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        console.log(values);
+
+        setValues({
+            login: "",
+            password: "",
+        })
     }
     return(
-        <form>
+        <form onSubmit={handleSubmit}>
             <input type="text" name="login" value={values.login} onChange={handleChange} />
             <input type="password" name="password" value={values.password} onChange={handleChange} />
             <button type='submit'>Login</button>
